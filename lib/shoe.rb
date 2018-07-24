dcb29685afbafa0d6f4f5e0b92f0abf65aa2e129
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-24 23:54:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,28 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
+    
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq{|b| BRANDS << b}
+    #binding.pry
+    
   end
+  
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  
+  # if brand name is included in BRANDS array, desregard
+    #iteration or BRANDS array 
+     
 end
